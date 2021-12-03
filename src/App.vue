4172f233a41d31b315d9d4303c7eb4948868e117
zajnemzaj <dezsoj@gmail.com>
2021-12-03 10:50:40 +0100

    ready for next chapter

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,28 @@
 <template>
-  <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick">click me</button>
+    <h1>{{ title }}</h1>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data() {
-      return {
-          title: 'My First Vue App :)'
-      }
-  },
-  methods: {
-      handleClick() {
-          console.log(this.$refs.name);
-          this.$refs.name.classList.add('active')
-          this.$refs.name.focus()
-      }
-  }
+    name: 'App',
+    data() {
+        return {
+            title: 'My First Vue App :)'
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
+
 h1 {
     border-bottom: 1px solid #ddd;
     display: inline-block;
