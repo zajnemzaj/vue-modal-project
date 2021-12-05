3812521248e45f00ed665a6ef7fad855131b0c65
zajnemzaj <dezsoj@gmail.com>
2021-12-05 23:31:44 +0100

    binding data to props

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <h1>{{ title }}</h1>
-    <Modal header="Sign up for the Giveaway!" text="GRab..."/>
+    <Modal :header="header" :text="text"/>
 </template>
 
 <script>
@@ -11,7 +11,9 @@ export default {
     components: { Modal },
     data() {
         return {
-            title: 'My First Vue App :)'
+            title: 'My First Vue App :)',
+            header: 'Sign up for the Giveaway!',
+            text: 'Grab your ninja for half price!'
         }
     }
 }
