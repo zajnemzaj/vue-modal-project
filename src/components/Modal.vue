5e0d8cbd1107b2c04af4735351fdcaeda69857d7
zajnemzaj <dezsoj@gmail.com>
2021-12-05 22:59:42 +0100

    scoped style with css selector (better performance)

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -7,7 +7,7 @@
     </div>
 </template>
 
-<style scoped>
+<style>
     .modal {
         width: 400px;
         padding: 20px;
@@ -22,7 +22,7 @@
         width: 100%;
         height: 100%;
     }
-    h1 {
+    .modal h1 {
         color: #03cfb4;
         border: none;
         padding: 0;
