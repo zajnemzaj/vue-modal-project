bfb77654bf56c40b6fba93ae3376dee77e577c41
zajnemzaj <dezsoj@gmail.com>
2021-12-05 22:51:41 +0100

    creating modal component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
     <h1>{{ title }}</h1>
+    <Modal />
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
+
 export default {
     name: 'App',
+    components: { Modal },
     data() {
         return {
             title: 'My First Vue App :)'
