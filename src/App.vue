e5b26ee945bafd43dffefac99b0dd76fee2b8590
zajnemzaj <dezsoj@gmail.com>
2021-12-06 09:54:19 +0100

    challange: creating a second button, to open an other modal, using the same modal with different template (slot)

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,18 @@
         <p>Grab your ninja swag for half price!</p>
     </Modal>
 </div>
+<div v-if="showModalTwo">
+    <Modal theme="sale" @close="toggleModalTwo">
+        <template v-slot:links>
+            <a href="#">sign up now</a>
+            <a href="#">more info</a>
+        </template>
+        <h1>Great Success!</h1>
+        <p>This is the second modal with red background</p>
+    </Modal>
+</div>
 <button @click.shift="toggleModal">open modal (shift)</button>
+<button @click="toggleModalTwo">open 2nd red modal</button>
 </template>
 
 <script>
@@ -27,12 +38,16 @@ export default {
             title: 'My First Vue App :)',
             header: 'Sign up for the Giveaway!',
             text: 'Grab your ninja for half price!',
-            showModal: false
+            showModal: false,
+            showModalTwo: false
         }
     },
     methods: {
         toggleModal() {
             this.showModal = !this.showModal
+        },
+        toggleModalTwo() {
+            this.showModalTwo = !this.showModalTwo
         }
     }
 }
