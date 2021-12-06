2832ed35195fad81d6338bc64d65ad7692fb4afd
zajnemzaj <dezsoj@gmail.com>
2021-12-06 10:25:46 +0100

    teleporting component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 <h1>{{ title }}</h1>
 <p>Welcome...</p>
 
-<div v-if="showModal">
+<teleport to="#modals" v-if="showModal">
     <Modal theme="" @close="toggleModal">
         <template v-slot:links>
             <a href="#">sign up now</a>
@@ -11,14 +11,14 @@
         <h1>Ninja Giveaway!</h1>
         <p>Grab your ninja swag for half price!</p>
     </Modal>
-</div>
+</teleport>
 
-<div v-if="showModalTwo">
+<teleport to="#modals" v-if="showModalTwo">
     <Modal theme="sale" @close="toggleModalTwo">
         <h1>Great Success!</h1>
         <p>This is the second modal with red background</p>
     </Modal>
-</div>
+</teleport>
 
 <button @click.shift="toggleModal">open modal (shift)</button>
 <button @click="toggleModalTwo">open 2nd red modal</button>
@@ -51,7 +51,7 @@ export default {
 </script>
 
 <style>
-#app {
+#app, #modals {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
