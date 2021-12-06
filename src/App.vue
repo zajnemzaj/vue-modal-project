c4ac100e035e0af1ea6acbef957bf1d7cb6ca5f8
zajnemzaj <dezsoj@gmail.com>
2021-12-06 08:57:12 +0100

    passing slot into custom component and use that slot inside a component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
 <h1>{{ title }}</h1>
 <p>Welcome...</p>
 <div v-if="showModal">
-    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+    <Modal theme="sale" @close="toggleModal">
+        <h1>Ninja Giveaway!</h1>
+        <p>Grab your ninja swag for half price!</p>
+    </Modal>
 </div>
 <button @click.shift="toggleModal">open modal (shift)</button>
 </template>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,15 +1,14 @@
 <template>
     <div class="backdrop" @click.self="closeModal">
         <div class="modal" :class="{ sale: theme === 'sale' }">
-            <h1>{{ header }}</h1>
-            <p>{{ text }}</p>
+            <slot></slot>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['header', 'text', 'theme'],
+        props: ['theme'],
         methods: {
             closeModal() {
                 this.$emit('close')
